dbf66d1b938d9f9705ea31ab51fe6d924990b517
DizelPizell <126786183+DizelPizell@users.noreply.github.com>
2024-08-05 18:00:30 +0300

    fix

--- js1.3/compareWithPrecision.js
+++ js1.3/compareWithPrecision.js
@@ -1,10 +1,10 @@
 function compareWithPrecision(num1, num2, num3){
     let IntResult = num1 - num2;
-    if(IntResult <= num3){
-        console.log(true);
+    if(Math.abs(IntResult) <= num3){
+        return true;
     }
     else{
-        console.log(false);
+        return false;
     }
 }
 
