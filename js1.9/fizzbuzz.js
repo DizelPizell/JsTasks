772b2ed00295f4abc4be0e25d51c7d40d8bd03d2
DizelPizell <126786183+DizelPizell@users.noreply.github.com>
2024-08-06 17:07:46 +0300

    1.9Fix

--- js1.9/fizzbuzz.js
+++ js1.9/fizzbuzz.js
@@ -1,12 +1,12 @@
 function fizzbuzz(a){
+    if(a % 3 == 0 && a % 5 == 0){
+        return 'FizzBuzz';
+    }
     if(a % 3 == 0){
         return 'Fizz';
     }
     if(a % 5 == 0){
         return 'Buzz';
-    }
-    if(a % 3 == 0 && a % 5 == 0){
-        return 'FizzBuzz';
     }
     else {
         return a;
@@ -16,3 +16,5 @@ function fizzbuzz(a){
 console.log(fizzbuzz(9));
 console.log(fizzbuzz(15));
 console.log(fizzbuzz(7));
+console.log(fizzbuzz(10));
+console.log(fizzbuzz(30));
