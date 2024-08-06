19999aeddbf66a0f117700da0e66d88cd9a35d78
DizelPizell <126786183+DizelPizell@users.noreply.github.com>
2024-08-06 17:03:38 +0300

    js1.6Fix

--- js1.6/wrapInParagraph.js
+++ js1.6/wrapInParagraph.js
@@ -1,12 +1,15 @@
-function wrapInParagraph(text){
+function wrapInParagraph(text) {
     const lines = text.split('\n');
-    return lines.map(line => '<p>${ line }</p>').join('\n');
+    return lines.map(line => `<p>${ line }</p>`).join('\n');
 }
 
-const text1 = 'Some\nsimple multiline\ntext';
+const text1 = `Some
+simple multiline
+text`;
+
+console.log(wrapInParagraph(text1));
 
 const text2 = 'some\ntext';
 
-console.log(wrapInParagraph(text1));
 console.log(wrapInParagraph(text2));
 
