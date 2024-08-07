e61e2622334943f5fdf70ef5c5abd1241a56bbe3
Daniil Karpov <kaartemij@gmail.com>
2024-08-07 14:37:36 +0300

    js1.12fix

--- js1.12/findAllIdx.js
+++ js1.12/findAllIdx.js
@@ -5,14 +5,12 @@ function findAllIdx(arr, value){
             index.push(i);
         }
     }
-    
-    // Возвращаем массив индексов
+
     return index;
 }
 
-// Примеры использования:
-console.log(findAllIdx([1, 0, 1, 0, 0, 1], 0)); // [1, 3, 4]
-console.log(findAllIdx([1, 1], 0));            // []
-console.log(findAllIdx([1, 2, 3, 4, 2], 2));   // [1, 4]
-console.log(findAllIdx([1, 2, 3, 4, 5], 5));   // [4]
-console.log(findAllIdx([1, 2, 3, 4, 5], 6));   // []
+console.log(findAllIdx([1, 0, 1, 0, 0, 1], 0));
+console.log(findAllIdx([1, 1], 0));            
+console.log(findAllIdx([1, 2, 3, 4, 2], 2));   
+console.log(findAllIdx([1, 2, 3, 4, 5], 5));   
+console.log(findAllIdx([1, 2, 3, 4, 5], 6));  
