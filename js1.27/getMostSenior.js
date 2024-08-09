72f943958f23e20d5216cbc099bf200165bc87c3
DizelPizell <126786183+DizelPizell@users.noreply.github.com>
2024-08-09 16:36:48 +0300

    js1.27

---
+++ js1.27/getMostSenior.js
@@ -0,0 +1,14 @@
+function getMostSenior(people = []) {
+    const maxAge = Math.max(...people.map(person => person.age));
+    return people.filter(person => person.age === maxAge);
+}
+
+const data = [
+    { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+    { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+    { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+    { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+];
+
+const result = getMostSenior(data);
+console.log(result);
