0327a1c42f85bda5094f0cb82f9bf1247898125e
Daniil Karpov <kaartemij@gmail.com>
2024-08-14 16:40:33 +0300

    js1.39

---
+++ js1.39/ObjectCreate.js
@@ -0,0 +1,34 @@
+function objectCreate(prototype, properties) {
+    const obj = {};
+    Object.setPrototypeOf(obj, prototype);
+    if (properties !== undefined) {
+        Object.defineProperties(obj, properties);
+    }
+
+    return obj;
+}
+
+const A = {
+    objectName: 'Object A',
+    getObjectName: function() {
+        return `This is ${this.objectName}!`;
+    },
+};
+
+const B = objectCreate(A, {
+    objectName: {
+        value: 'Object B',
+        writable: true,
+        configurable: true,
+        enumerable: true,
+    },
+});
+
+console.log(A.getObjectName());
+console.log(B.getObjectName());
+
+console.log(A.hasOwnProperty('getObjectName'));
+console.log(A.hasOwnProperty('objectName'));
+
+console.log(B.hasOwnProperty('getObjectName'));
+console.log(B.hasOwnProperty('objectName'));
